942326241b8c6b01b1356c7423a2a6c149ae99ec
SenhW <senhwang3@gmail.com>
2015-12-06 00:50:37 -0500

    useful functions

---
+++ get_features.py
@@ -0,0 +1,30 @@
+import sys
+import json
+
+business_data = 'yelp_academic_dataset_business.json'
+tip_data = 'yelp_academic_dataset_tip.json'
+checking_data = 'yelp_academic_dataset_checkin.json'
+user_data = 'yelp_academic_dataset_user.json'
+review_data = 'yelp_academic_dataset_review.json'
+
+
+def get_business_ids(category):
+    with open(business_data) as businesses:
+        business_ids = []
+        for business in businesses:
+            business = json.loads(business)
+            if category in business['categories']:
+                business_ids.append(business['business_id'])
+    return business_ids
+
+def find_average_rating():
+    total_star_rating = 0.0
+    total_businesses = 0.0
+    with open(business_data) as businesses:
+        for business in businesses:
+            business = json.loads(business)
+            total_star_rating += business['stars']
+            total_businesses += 1
+    return total_star_rating / total_businesses
+
+print find_average_rating()
